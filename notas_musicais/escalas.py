eebeb210897d8a6893b9a95cbc4fb2d6473ea814
Victor Enrique Cavalheri Graciano <victor.graciano@duartecon.local>
2023-06-15 17:03:56 -0300

    testes

--- notas_musicais/escalas.py
+++ notas_musicais/escalas.py
@@ -1,8 +1,8 @@
-NOTAS = 'C C# D D# E E# F F# G G# A A# B B'.split()
+NOTAS = 'C C# D D# E F F# G G# A A# B'.split()
 ESCALAS = {'maior': (0, 2, 4, 5, 7, 9, 11)}
 
 
-def escalas(tonica, tonalidade):
+def escalas(tonica: str, tonalidade: str) -> dict[str, list[str]]:
     """
     GERA UMA ESCALA A PARTIR DE UMA TONICA E UMA TONALIDADE
 
@@ -19,17 +19,17 @@ def escalas(tonica, tonalidade):
 
     Examples:
         >>> escalas('C','maior')
-        {'notas':['C','D','E','F','G','A','B'], 'graus':['I','II','III','IV','V','VI','VII']}
+        {'notas': ['C', 'D', 'E', 'F', 'G', 'A', 'B'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
 
-        >>> esalas('a','maior')
-        {'notas':['A','B','C','D','E','F','G'], 'graus':['I','II','III','IV','V','VI','VII']}
+        >>> escalas('a','maior')
+        {'notas': ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
     """
     tonica = tonica.upper()
     try:
         intervalos = ESCALAS[tonalidade]
         tonica_pos = NOTAS.index(tonica)
     except ValueError:
-        raise ValueError(f'Essa nota não existe, tente uma dessas  {NOTAS}')
+        raise ValueError(f'Essa nota não existe, tente uma dessas {NOTAS}')
     except KeyError:
         raise KeyError(
             f'Essa escala não foi implementada, tente uma dessas {list(ESCALAS.keys())}'
@@ -38,6 +38,6 @@ def escalas(tonica, tonalidade):
 
     for intervalo in intervalos:
         nota = (tonica_pos + intervalo) % 12
-        temp.append(NOTAS[intervalo])
+        temp.append(NOTAS[nota])
 
     return {'notas': temp, 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
--- tests/test_escalas.py
+++ tests/test_escalas.py
@@ -4,7 +4,7 @@ AAA - 3A - 3A
 Arange -  Act - Assets!
 Arrumar - Agir -  Garantir!
 """
-from pytest import raises
+from pytest import mark, raises
 
 from notas_musicais.escalas import ESCALAS, NOTAS, escalas
 
@@ -23,9 +23,9 @@ def test_escalas_notas_minusculas():
 
 def test_mensagem_erro():
     tonica = 'X'
-    tonalidade = 'menor'
+    tonalidade = 'maior'
 
-    mensagem_erro = f'Essa nota não existe, tente uma dessas , {NOTAS}'
+    mensagem_erro = f'Essa nota não existe, tente uma dessas {NOTAS}'
 
     with raises(ValueError) as error:
         escalas(tonica, tonalidade)
@@ -37,9 +37,17 @@ def test_escala_nao_existe():
     tonica = 'c'
     tonalidade = 'tonalidade'
 
-    mensagem_erro = f'Essa escala não foi implementada, tente uma dessas {list(ESCALAS.keys)}'
+    mensagem_erro = f'Essa escala não foi implementada, tente uma dessas {list(ESCALAS.keys())}'
 
     with raises(KeyError) as error:
         escalas(tonica, tonalidade)
 
     assert mensagem_erro == error.value.args[0]
+
+
+@mark.parametrize(
+    'tonica, esperado', [('C', ['C', 'D', 'E', 'F', 'G', 'A', 'B'])]
+)
+def test_notas_corretas(tonica, esperado):
+    resultado = escalas(tonica, 'maior')
+    assert resultado['notas'] == esperado
